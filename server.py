43069bbe377cdec1d4a163b6ff6b98f66edf8b8d
KevenParent <keven.parent.2@ulaval.ca>
2019-03-14 15:59:42 -0400

    Déplacement de l'ouverture de la cam

--- server.py
+++ server.py
@@ -12,11 +12,18 @@ def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	s.bind(('', 15555))
-	img_counter = 0
 	s.listen(1)
 
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(18,GPIO.OUT)
+	
+	try:
+		cam = cv2.VideoCapture(0)
+                cam.set(3, 320);
+                cam.set(4, 240);
+		encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+	except:
+		pass
 
 	try:
 		ser = serial.Serial('/dev/ttyACM0', 1000000)
@@ -33,26 +40,18 @@ def main():
 		coor = client.recv(255)
 		if coor == 'sendPosition':
 			position = client.recv(255)
-			print('Recu:{}'.format(position))
 			data = bytearray(position,'utf-8')
 			ser.write(data)
 			ser.flush()
+
         	elif coor == 'getImage':
-                	cam = cv2.VideoCapture(0)
-                	print("Capture video ok")
-                	cam.set(3, 320);
-                	cam.set(4, 240);
-                	encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
                 	ret, frame = cam.read()
                 	result, frame = cv2.imencode('.jpg', frame, encode_param)
-                
                 	data = pickle.dumps(frame, 0)
                 	size = len(data)
                 	print("{}: {}".format(img_counter, size))
                 	client.sendall(struct.pack(">L", size) + data)
-                	img_counter += 1
                 	print("Send fonctionne")
-                	cam.release()
 	
 		elif coor == 'deconnect':
 			#GPIO.output(18,GPIO.LOW)
